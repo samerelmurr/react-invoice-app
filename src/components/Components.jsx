b267aac37529921be789465b5bb461659da4515d
Samer  El-Murr <snjmurr@gmail.com>
2022-11-09 19:02:14 +0200

    Invoice application

---
+++ src/components/Components.jsx
@@ -0,0 +1,19 @@
+import Header from './Header/Header';
+import MainDetails from './MainDetails/MainDetails';
+import ClientDetails from './ClientDetails/ClientDetails';
+import Date from './Date/Date';
+import Table from './Table/Table';
+import Notes from './Notes/Notes';
+import TableForm from './TableForm/TableForm';
+import Footer from './Footer/Footer';
+
+export const Components = {
+    Header, 
+    MainDetails, 
+    ClientDetails, 
+    Date, 
+    Table, 
+    TableForm, 
+    Notes, 
+    Footer
+};
---
+++ src/components/MainDetails/MainDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MainDetails = ({ name, address }) => {
+  return (
+    <>
+      <section className="flex flex-col items-end justify-end">
+        <h2 className="font-bold text-3xl uppercase mb-1">{name}</h2>
+        <p>{address}</p>
+      </section>
+    </>
+  )
+}
+
+export default MainDetails
